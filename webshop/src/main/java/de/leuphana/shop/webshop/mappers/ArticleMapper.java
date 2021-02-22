031362ee65ee8a5506f128eff4fb58dc8b9bf28e
karan <karan.bruellau1997@gmail.com>
2021-02-22 17:41:52 +0100

    temporarily removed mappers for bugfix

--- webshop/src/main/java/de/leuphana/shop/webshop/mappers/ArticleMapper.java
+++ webshop/src/main/java/de/leuphana/shop/webshop/mappers/ArticleMapper.java
@@ -1,12 +1,12 @@
-package de.leuphana.shop.webshop.mappers;
-
-import de.leuphana.shop.webshop.domain.Article;
-import de.leuphana.shop.webshop.model.ArticleDto;
-import org.mapstruct.Mapper;
-
-@Mapper // for mapstruct -> ArticleMapperImpl gets generated automatically (in target folder under mappers)
-public interface ArticleMapper {
-
-    ArticleDto articleToArticleDto(Article article);
-    Article articleDtoToArticle(ArticleDto articleDto);
-}
+//package de.leuphana.shop.webshop.mappers;
+//
+//import de.leuphana.shop.webshop.domain.Article;
+//import de.leuphana.shop.webshop.model.ArticleDto;
+//import org.mapstruct.Mapper;
+//
+//@Mapper // for mapstruct -> ArticleMapperImpl gets generated automatically (in target folder under mappers)
+//public interface ArticleMapper {
+//
+//    ArticleDto articleToArticleDto(Article article);
+//    Article articleDtoToArticle(ArticleDto articleDto);
+//}
